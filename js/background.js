4bae1ac886fad3dbd05bfccd639cadb2ba77ea1f
Uchi Uchibeke <u.i@usask.ca>
2016-11-12 14:05:33 -0500

    get the text content of a job posting

--- js/background.js
+++ js/background.js
@@ -9,23 +9,63 @@ chrome.browserAction.setBadgeBackgroundColor({
 	color : 'black'
 });
 
-chrome.browserAction.onClicked.addListener(function(aTab) {
-	chrome.tabs.query({'url': 'http://chilloutandwatchsomecatgifs.com/'}, (tabs) => {
-		if (tabs.length === 0) {
-			// There is no catgif tab!
-			chrome.tabs.create({
-				'url' : 'http://chilloutandwatchsomecatgifs.com/',
-				'active' : true
-			});
-		} else {
-			// Do something here…
-			chrome.tabs.query({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true}, (active) => {
-				if (active.length === 0) {
-					chrome.tabs.update(tabs[0].id, {
-						'active' : true
-					});
-				}
-			});
-		}
+// chrome.browserAction.onClicked.addListener(function(aTab) {
+// chrome.tabs.query({'url': 'http://chilloutandwatchsomecatgifs.com/'}, (tabs) => {
+// if (tabs.length === 0) {
+// chrome.tabs.create({
+// 'url' : 'http://chilloutandwatchsomecatgifs.com/',
+// 'active' : true
+// });
+// } else {
+// chrome.tabs.query({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true}, (active) => {
+// if (active.length === 0) {
+// chrome.tabs.update(tabs[0].id, {
+// 'active' : true
+// });
+// }
+// });
+// }
+// });
+// });
+
+var pageText,
+    pageUrl,
+    pageTitle = "",
+    loadStatus = "";
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	console.log('Injecting content script(s)');
+
+	var selectedTab = "";
+	chrome.tabs.query({
+		active : true,
+		lastFocusedWindow : true
+	}, function(tabs) {
+		var selectedTab = tabs[0];
+		console.log(selectedTab);
+
+		pageUrl = selectedTab.url;
+		pageTitle = selectedTab.title;
+		loadStatus = selectedTab.status;
+
+		chrome.tabs.executeScript(selectedTab.id, {
+			code : 'document.body.innerText;' //Gives you all the text on the page
+			// code : 'document.body.outerHTML;'  //Gives you the whole HTML of the page
+		}, function(response) {
+			pageText = response[0];
+			console.log(pageText);
+			alert(pageText);
+			console.log(pageUrl);
+			console.log(pageTitle);
+		});
+
 	});
-}); 
+});
+
+function getText() {
+	return document.body.innerText;
+}
+
+function receiveText(resultsArray) {
+	console.log(resultsArray[0]);
+}
